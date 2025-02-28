b782efab6f744537b51ca42567dbe7130b852523
aamna-ansari <aamnansari29@gmail.com>
2025-02-28 13:26:05 +0500

    add functions for multiplication table and vowel counting in Loops.js; create new Arraymethods.js file

--- 04_Frontend/JS/JS_Practice/Loops.js
+++ 04_Frontend/JS/JS_Practice/Loops.js
@@ -1,10 +1,36 @@
 // Task 1: Sum of First N Natural Numbers
 function sumOfN(n) {
-    var sum = 0;
-    for (var i = 1; i <= n; i++) {
-        sum += i;
-    }
-    return sum;
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+console.log(sumOfN(6));
+
+
+// Task 2: Multiplication Table (Without Using an Array)
+function printMultiplicationTable(n) {
+  let i = 1;
+  while (i <= 10) {
+    console.log(n + " * " + i + " = " + n * i);
+    i++;
+  }
 }
 
-// 
+printMultiplicationTable(2);
+
+// Task 3: Count Vowels in a String
+function countVowels(str) {
+  let count = 0;
+  let vowels = "aeiouAEIOU";
+
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countVowels("AaMna"));
