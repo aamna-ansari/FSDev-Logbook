6d5f75945c2604e05c773533a3167c11ee02eb9b
aamna-ansari <aamnansari29@gmail.com>
2025-02-28 12:24:21 +0500

    add JavaScript functions for string to number conversion, boolean flipping, and type identification

---
+++ 04_Frontend/JS/JS_Practice/exercise1.js
@@ -0,0 +1,26 @@
+
+// 📌TASK 1
+// Write a function stringToNumber that takes a string input and tries to convert it to a number. If the conversion fails, return "Not a number".
+function stringToNumber(input) {
+    const number = Number(input);
+    return isNaN(number) ? 'Not a Number' : number
+}
+
+console.log(stringToNumber("2"))
+
+// 📌TASK 2
+
+// Write a function flipBoolean that takes any input and converts it to its boolean equivalent, then flips it. For example, true becomes false, 0 becomes true, etc.
+function flipBoolean(input) {
+    return !Boolean(input);
+}
+
+console.log(flipBoolean(true));
+
+// 📌TASK 3
+// Write a function whatAmI that takes an input and returns a string describing its type after conversion. If it's a number, return "I'm a number!", if it's a string, return "I'm a string!"
+
+function whatAmI(input) {
+}
+
+ 
