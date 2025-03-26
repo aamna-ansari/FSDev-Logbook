2f32edeeb89333b472e63b39ebe13735c8aed15a
aamna-ansari <aamnansari29@gmail.com>
2025-03-26 19:53:23 +0500

    update comments in TODO List JavaScript for clarity on steps

--- 04_Frontend/JS/Projects/TODO_List/index.js
+++ 04_Frontend/JS/Projects/TODO_List/index.js
@@ -3,6 +3,10 @@ let todoList = document.querySelector('#todoList');
 let todoButton = document.querySelector('#todoButton');
 // Step 1: click on button to add todo by using event listener
 // Step 2: Get the value of the input field
-// step 3: 
+// step 3: add the value of the input field to the li
+// step 4: clear the input field
+// step 5: create a delete button
+// step 6: create a check button
+// step 7: create a function to delete and check the todo
 
 let getInputValue = todoInput.value;
