40679698791d1bdb3b21ea3803588b2099a1a88d
aamna-ansari <aamnansari29@gmail.com>
2025-03-26 19:52:05 +0500

    add id to todo button and select it in JavaScript

--- 04_Frontend/JS/Projects/TODO_List/index.js
+++ 04_Frontend/JS/Projects/TODO_List/index.js
@@ -1,7 +1,7 @@
 let todoInput = document.querySelector('#todoInput');
 let todoList = document.querySelector('#todoList');
+let todoButton = document.querySelector('#todoButton');
 // Step 1: click on button to add todo by using event listener
-
 // Step 2: Get the value of the input field
 // step 3: 
 
