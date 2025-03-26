80e6f7c27902248a25778ab9d7e55475ef95a72a
aamna-ansari <aamnansari29@gmail.com>
2025-03-26 19:49:06 +0500

    .

---
+++ 04_Frontend/JS/Projects/TODO_List/index.js
@@ -0,0 +1,8 @@
+let todoInput = document.querySelector('#todoInput');
+let todoList = document.querySelector('#todoList');
+// Step 1: click on button to add todo by using event listener
+
+// Step 2: Get the value of the input field
+// step 3: 
+
+let getInputValue = todoInput.value;
