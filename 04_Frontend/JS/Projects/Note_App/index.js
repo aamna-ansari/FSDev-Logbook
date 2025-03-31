c49d35e1c6adbc62986ba72cfa0b340e93021f11
aamna-ansari <aamnansari29@gmail.com>
2025-03-31 21:26:41 +0500

    add event listener to addNoteButton

--- 04_Frontend/JS/Projects/Note_App/index.js
+++ 04_Frontend/JS/Projects/Note_App/index.js
@@ -2,3 +2,10 @@ let noteTitle = document.querySelector("#noteTitle");
 let noteInput = document.querySelector("#noteInput");
 let addNoteButton = document.querySelector("#addNote");
 let notesList =  document.querySelector("#notesList");
+
+addNoteButton.addEventListener('click',()=>{
+let getNoteTitle = noteTitle.value;
+let getNoteInput = noteInput.value;
+if (getNoteTitle === '' || getNoteInput === '') return;
+
+})
