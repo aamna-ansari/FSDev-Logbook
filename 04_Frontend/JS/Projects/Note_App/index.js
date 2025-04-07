368eb3e924642ef98685f3a36c98114f0a19cf44
aamna-ansari <aamnansari29@gmail.com>
2025-04-07 21:14:43 +0500

    refactor note rendering for improved styling consistency and text wrapping

--- 04_Frontend/JS/Projects/Note_App/index.js
+++ 04_Frontend/JS/Projects/Note_App/index.js
@@ -9,7 +9,7 @@ addNoteButton.addEventListener("click", () => {
   if (getNoteTitle === "" || getNoteInput === "") return;
   let noteRender = document.createElement("div");
   noteRender.className =
-    "relative mb-6 bg-white p-6 rounded-lg shadow transition-colors";
+    "relative mx-h-60 mb-6 bg-white p-6 rounded-lg shadow transition-colors";
   noteRender.innerHTML = `<div class="absolute top-3 right-3 flex gap-2">
     <button class="text-xl text-gray-500 hover:text-blue-700 cursor-pointer">
     <i class="fa-solid fa-pen-to-square"></i>
@@ -18,7 +18,8 @@ addNoteButton.addEventListener("click", () => {
     <button class=" text-xl deleteBtn text-gray-500 hover:text-red-700 cursor-pointer"><i class="fa-regular fa-circle-xmark"></i></button>
     </div>
     <h2 class="text-lg font-bold">${getNoteTitle}</h2>
-    <p class="text-gray-700 whitespace-pre-wrap">${getNoteInput}</p> `;
+    <p class="text-gray-700 whitespace-pre-wrap break-words">${getNoteInput}</p>
+ `;
   notesList.appendChild(noteRender);
   noteTitle.value = "";
   noteInput.value = "";
