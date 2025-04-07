aad9125826de3802ac87f41af171639db9626c9b
aamna-ansari <aamnansari29@gmail.com>
2025-04-07 20:32:03 +0500

    refactor event listener for addNoteButton to improve readability and maintainability

--- 04_Frontend/JS/Projects/Note_App/index.js
+++ 04_Frontend/JS/Projects/Note_App/index.js
@@ -1,11 +1,22 @@
 let noteTitle = document.querySelector("#noteTitle");
 let noteInput = document.querySelector("#noteInput");
 let addNoteButton = document.querySelector("#addNote");
-let notesList =  document.querySelector("#notesList");
+let notesList = document.querySelector("#notesList");
 
-addNoteButton.addEventListener('click',()=>{
-let getNoteTitle = noteTitle.value;
-let getNoteInput = noteInput.value;
-if (getNoteTitle === '' || getNoteInput === '') return;
-
-})
+addNoteButton.addEventListener("click", () => {
+  let getNoteTitle = noteTitle.value;
+  let getNoteInput = noteInput.value;
+  if (getNoteTitle === "" || getNoteInput === "") return;
+  let noteRender = document.createElement("div");
+  noteRender.className =
+    "relative mb-6 bg-white p-6 rounded-lg shadow transition-colors";
+  noteRender.innerHTML = `<div class="absolute top-2 right-2 flex gap-2 opacity-0                                                                hover:opacity-100 transition-opacity">
+    <button class="deleteBtn text-red-500 hover:text-red-700 cursor-pointer">❌</button>
+    </div>
+    <h2 class="text-lg font-bold">${getNoteTitle}</h2>
+                            <p class="text-gray-700">${getNoteInput}</p>
+                            `;
+  notesList.appendChild(noteRender);
+  noteTitle.value = "";
+  noteInput.value = "";
+});
