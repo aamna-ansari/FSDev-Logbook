707e3834a3fd60901726288495dc635814d2c072
aamna-ansari <aamnansari29@gmail.com>
2025-04-07 21:03:29 +0500

    refactor button styling for improved visual consistency in note rendering

--- 04_Frontend/JS/Projects/Note_App/index.js
+++ 04_Frontend/JS/Projects/Note_App/index.js
@@ -11,14 +11,14 @@ addNoteButton.addEventListener("click", () => {
   noteRender.className =
     "relative mb-6 bg-white p-6 rounded-lg shadow transition-colors";
   noteRender.innerHTML = `<div class="absolute top-3 right-3 flex gap-2">
-    <button class="text-2xl text-gray-500 hover:text-blue-700 cursor-pointer">
+    <button class="text-xl text-gray-500 hover:text-blue-700 cursor-pointer">
     <i class="fa-solid fa-pen-to-square"></i>
     </button>
-
-            <button class=" text-2xl deleteBtn text-gray-500 hover:text-red-700 cursor-pointer"><i class="fa-regular fa-circle-xmark"></i></button>
+    
+    <button class=" text-xl deleteBtn text-gray-500 hover:text-red-700 cursor-pointer"><i class="fa-regular fa-circle-xmark"></i></button>
     </div>
     <h2 class="text-lg font-bold">${getNoteTitle}</h2>
-    <p class="text-gray-700">${getNoteInput}</p> `;
+    <p class="text-gray-700 whitespace-pre-wrap">${getNoteInput}</p> `;
   notesList.appendChild(noteRender);
   noteTitle.value = "";
   noteInput.value = "";
